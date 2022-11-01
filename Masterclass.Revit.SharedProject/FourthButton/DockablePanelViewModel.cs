c7eb38b05f7e35ade2f96a9f6b04a0a1d07c55fa
Konrad Sobon <sobon.konrad@gmail.com>
2022-11-01 13:42:38 -0500

    read excel

--- Masterclass.Revit.SharedProject/FourthButton/DockablePanelViewModel.cs
+++ Masterclass.Revit.SharedProject/FourthButton/DockablePanelViewModel.cs
@@ -1,12 +1,23 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Masterclass.Revit.Utilities;
+using OfficeOpenXml;
 
 namespace Masterclass.Revit.FourthButton
 {
     public class DockablePanelViewModel : ViewModelBase
     {
         public RelayCommand LoadRequirements { get; set; }
+
+        private ObservableCollection<RequirementWrapper> _requirements = new ObservableCollection<RequirementWrapper>();
+        public ObservableCollection<RequirementWrapper> Requirements
+        {
+            get { return _requirements;}
+            set { _requirements = value; RaisePropertyChanged(() => Requirements); }
+        }
 
         public DockablePanelViewModel()
         {
@@ -19,7 +30,27 @@ namespace Masterclass.Revit.FourthButton
             if (string.IsNullOrWhiteSpace(filePath))
                 return;
 
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
+            var requirements = new List<RequirementWrapper>();
+            var existingFile = new FileInfo(filePath);
+            using (var package = new ExcelPackage(existingFile))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                var rowCount = worksheet.Dimension.End.Row;
+
+                for (var row = 2; row <= rowCount; row++)
+                {
+                    var fn = worksheet.Cells[row, 1].Value.ToString();
+                    var ft = worksheet.Cells[row, 2].Value.ToString();
+                    var count = int.Parse(worksheet.Cells[row, 3].Value.ToString());
+                    var req = new RequirementWrapper(fn, ft, count);
+                    requirements.Add(req);
+                }
+            }
+
+            Requirements.Clear();
+            requirements.ForEach(x => Requirements.Add(x));
         }
     }
 }
---
+++ Masterclass.Revit.SharedProject/FourthButton/RequirementWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Masterclass.Revit.FourthButton
+{
+    public class RequirementWrapper
+    {
+        public string FamilyName { get; set; }
+        public string FamilyType { get; set; }
+        public int RequiredCount { get; set; }
+
+        public RequirementWrapper(string fn, string ft, int count)
+        {
+            FamilyName = fn;
+            FamilyType = ft;
+            RequiredCount = count;
+        }
+    }
+}
