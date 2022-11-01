a6b87309c6633dd376f166ed3064e6ddb388b17e
Konrad Sobon <sobon.konrad@gmail.com>
2022-11-01 14:14:06 -0500

    add requirement wrappers to the ui

---
+++ Masterclass.Revit.SharedProject/FourthButton/RequirementControl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Masterclass.Revit.FourthButton
+{
+    public sealed partial class RequirementControl
+    {
+        public RequirementControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Masterclass.Revit.SharedProject/FourthButton/RequirementWrapper.cs
+++ Masterclass.Revit.SharedProject/FourthButton/RequirementWrapper.cs
@@ -1,16 +1,37 @@
-﻿namespace Masterclass.Revit.FourthButton
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Masterclass.Revit.FourthButton
 {
-    public class RequirementWrapper
+    public class RequirementWrapper : INotifyPropertyChanged
     {
         public string FamilyName { get; set; }
         public string FamilyType { get; set; }
         public int RequiredCount { get; set; }
+
+        private int _placedCount;
+        public int PlacedCount
+        {
+            get { return _placedCount;}
+            set { _placedCount = value; RaisePropertyChanged(nameof(PlacedCount)); }
+        }
+
+        public string FamilyNameAndType
+        {
+            get { return $"{FamilyName} - {FamilyType}"; }
+        }
 
         public RequirementWrapper(string fn, string ft, int count)
         {
             FamilyName = fn;
             FamilyType = ft;
             RequiredCount = count;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
